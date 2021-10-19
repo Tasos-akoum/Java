19733e8c594cd61f481a3575fc9ac61b1fb69867
Tasos-Akoum <csd4582@csd.uoc.gr>
2021-10-19 15:54:42 -0400

    change_1

--- src/A11/DrawF.java
+++ src/A11/DrawF.java
@@ -10,7 +10,6 @@ class DrawF
 {
     static public void drawF(int L)
     {
-        // Comment
         for (int j = 0; j < L; j++) {
             System.out.print("*");
         }
---
+++ update.sh
@@ -0,0 +1,5 @@
+$yourname
+git add *
+git commit -am $yourname
+git push
+echo done
